e2f6e4ff3bc3bc04b0060036039636ac880219c7
Svetlana Efimova <svetlana.e08@gmail.com>
2021-08-25 21:07:08 +0300

    task 1 number of films

---
+++ task_1/script.js
@@ -0,0 +1,22 @@
+let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?');
+let lastSeenMovie, rate;
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
+
+if (typeof numberOfFilms === 'number' && !isNaN(numberOfFilms)) {
+    lastSeenMovie = prompt('Один из последних просмотренных фильмов?');
+    rate = prompt('На сколько оцените его?');
+    personalMovieDB.movies[lastSeenMovie] = rate;
+    lastSeenMovie = prompt('Один из последних просмотренных фильмов?');
+    rate = prompt('На сколько оцените его?');
+    personalMovieDB.movies[lastSeenMovie] = rate;
+    console.log(personalMovieDB);
+} else {
+    console.log('Необходимо указать число фильмов');
+}
