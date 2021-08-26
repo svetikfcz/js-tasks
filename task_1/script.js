a30b8140033ae0df67f7ee91d0e05289293d5609
Svetlana Efimova <svetlana.e08@gmail.com>
2021-08-26 21:35:03 +0300

    task 2 conditions

--- task_1/script.js
+++ task_1/script.js
@@ -1,5 +1,6 @@
+'use strict';
+
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?');
-let lastSeenMovie, rate;
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -9,14 +10,56 @@ const personalMovieDB = {
     privat: false,
 };
 
-if (typeof numberOfFilms === 'number' && !isNaN(numberOfFilms)) {
-    lastSeenMovie = prompt('Один из последних просмотренных фильмов?');
+for (let i = 0; i < 2; i++) {
+    let lastSeenMovie = prompt('Один из последних просмотренных фильмов?'),
     rate = prompt('На сколько оцените его?');
-    personalMovieDB.movies[lastSeenMovie] = rate;
-    lastSeenMovie = prompt('Один из последних просмотренных фильмов?');
-    rate = prompt('На сколько оцените его?');
-    personalMovieDB.movies[lastSeenMovie] = rate;
-    console.log(personalMovieDB);
-} else {
-    console.log('Необходимо указать число фильмов');
+    if (lastSeenMovie !== '' && rate !== '' && lastSeenMovie !== null && rate !== null && lastSeenMovie.length < 50) {
+        personalMovieDB.movies[lastSeenMovie] = rate;
+    } else {
+        console.log('Произошла ошибка');
+        i--;
+    }
 }
+
+console.log(personalMovieDB);
+
+if (personalMovieDB.count < 10) {
+    console.log('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('Вы классический зритель');
+} else if (personalMovieDB.count >= 30) {
+    console.log('Вы киноман');
+} else {
+    console.log('Произошла ошибка');
+}
+
+
+/* Последний пункт переписать цикл двумя способами
+Способ 1
+
+let i = 0;
+while (i < 2) {
+    i++;
+    let lastSeenMovie = prompt('Один из последних просмотренных фильмов?'),
+        rate = prompt('На сколько оцените его?');
+        if (lastSeenMovie !== '' && rate !== '' && lastSeenMovie !== null && rate !== null && lastSeenMovie.length < 50) {
+            personalMovieDB.movies[lastSeenMovie] = rate;
+        } else {
+            console.log('Произошла ошибка');
+            i--;
+        }    
+} */
+
+/* Способ 2
+let i = 0;
+do {
+    i++
+    let lastSeenMovie = prompt('Один из последних просмотренных фильмов?'),
+    rate = prompt('На сколько оцените его?');
+    if (lastSeenMovie !== '' && rate !== '' && lastSeenMovie !== null && rate !== null && lastSeenMovie.length < 50) {
+        personalMovieDB.movies[lastSeenMovie] = rate;
+    } else {
+        console.log('Произошла ошибка');
+        i--;
+    } 
+} while (i < 2); */
