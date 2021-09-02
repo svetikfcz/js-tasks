75be986d6cb97a311b0f0fd8278a4c1aebf7fe54
Svetlana Efimova <svetlana.e08@gmail.com>
2021-09-02 21:16:20 +0300

    final task with movies

--- movies/js/script.js
+++ movies/js/script.js
@@ -12,39 +12,101 @@
 
 5) Добавить нумерацию выведенных фильмов */
 
+/* Задания на урок:
+1) Реализовать функционал, что после заполнения формы и нажатия кнопки "Подтвердить" - 
+новый фильм добавляется в список. Страница не должна перезагружаться.
+Новый фильм должен добавляться в movieDB.movies.
+Для получения доступа к значению input - обращаемся к нему как input.value;
+P.S. Здесь есть несколько вариантов решения задачи, принимается любой, но рабочий.
+2) Если название фильма больше, чем 21 символ - обрезать его и добавить три точки
+3) При клике на мусорную корзину - элемент будет удаляться из списка (сложно)
+4) Если в форме стоит галочка "Сделать любимым" - в консоль вывести сообщение: 
+"Добавляем любимый фильм"
+5) Фильмы должны быть отсортированы по алфавиту */
+
 'use strict';
 
-const movieDB = {
-    movies: [
-        "Логан",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против..."
-    ]
-};
+document.addEventListener('DOMContentLoaded', () => {
+    const movieDB = {
+        movies: [
+            "Логан",
+            "Лига справедливости",
+            "Ла-ла лэнд",
+            "Одержимость",
+            "Скотт Пилигрим против..."
+        ]
+    };
+    
+    const adv = document.querySelectorAll('.promo__adv img'),
+        promo = document.querySelector('.promo__bg'),
+        genre = promo.querySelector('.promo__genre'),
+        movieList = document.querySelector('.promo__interactive-list'),
+        addForm = document.querySelector('form.add'),
+        addInput = document.querySelector('.adding__input'),
+        checkbox = addForm.querySelector('[type="checkbox"]');
 
-const adv = document.querySelectorAll('.promo__adv img'),
-    promo = document.querySelector('.promo__bg'),
-    genre = promo.querySelector('.promo__genre'),
-    movieList = document.querySelector('.promo__interactive-list');
+    addForm.addEventListener('submit', (event) => {
+        event.preventDefault();
 
-adv.forEach(item => {
-    item.remove();
-});
+        let newMovie = addInput.value;
 
-genre.textContent = 'драма';
+        if (newMovie) {
+            if (newMovie.length > 21) {
+                newMovie = `${newMovie.slice(0, 21)}...`;
+            }
+            movieDB.movies.push(newMovie);
 
-promo.style.backgroundImage = 'url("img/bg.jpg")';
+            if (checkbox.checked) {
+                console.log('Добавляем любимый фильм');
+            }
 
-movieList.innerHTML = '';
+            sortArr(movieDB.movies);
+            createMovieList(movieDB.movies, movieList);
+        }
 
-movieDB.movies.sort();
+        event.target.reset();
+    });
+    
+    const deleteAdv = (arr) => {
+        arr.forEach(item => {
+            item.remove();
+        });
+    };
+    
+    const makeChanges = () => {
+        genre.textContent = 'драма';
+    
+        promo.style.backgroundImage = 'url("img/bg.jpg")';
+    };
 
-movieDB.movies.forEach((movie, index) => {
-    movieList.innerHTML += `
-        <li class="promo__interactive-item">${index + 1}. ${movie}
-            <div class="delete"></div>
-        </li>
-    `;
+    const sortArr = (arr) => {
+        arr.sort();
+    };
+
+    function createMovieList(films, parent) {
+        parent.innerHTML = '';
+
+        sortArr(films);
+    
+        films.forEach((movie, index) => {
+            parent.innerHTML += `
+            <li class="promo__interactive-item">${index + 1}. ${movie}
+                <div class="delete"></div>
+            </li>
+        `;
+        });
+
+        document.querySelectorAll('.delete').forEach((deleteBtn, i) => {
+            deleteBtn.addEventListener('click', (e) => {
+                deleteBtn.parentElement.remove();
+                movieDB.movies.splice(i, 1);
+
+                createMovieList(films, parent);
+            });
+        });
+    }
+
+    deleteAdv(adv);
+    makeChanges();
+    createMovieList(movieDB.movies, movieList);
 });
